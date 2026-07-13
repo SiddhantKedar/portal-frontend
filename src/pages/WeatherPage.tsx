903e137ec1e8a11e8cbdb6d0e13ab77935cd2d9c
Siddhant Kedar <siddhant.kedar568@gmail.com>
2026-07-13 17:14:04 +0530

    removed extra padding

--- src/pages/WeatherPage.tsx
+++ src/pages/WeatherPage.tsx
@@ -482,7 +482,7 @@ export default function WeatherPage() {
   const deltaColor = delta > 10 ? 'text-[#e17100]' : delta < 0 ? 'text-black' : 'text-[#497d00]'
 
   return (
-    <div className="w-full max-w-[1152px] mx-auto px-4 sm:px-6 md:px-6 lg:px-6 pb-10">
+    <div className="w-full max-w-[1152px] mx-auto px-0 sm:px-6 md:px-6 lg:px-6 pb-10">
 
       {/* ============ HEADER ============ */}
       <header className="pb-5 flex flex-col md:flex-row md:items-start md:justify-between md:flex-wrap gap-3 md:gap-6">
